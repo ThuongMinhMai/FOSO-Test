685ac27d2126b548d78f3aed9e613dae833a384e
ThuongMinhMai <thuongmtmse170103@fpt.edu.vn>
2025-07-24 01:13:42 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { useRef } from 'react'
-import Footer from './components/layout/Footer.tsx'
-import Header from './components/layout/header/Header.tsx'
-import PromoBanner from './components/ui/banner/PromoBanner.tsx'
-import Sidebar from './components/layout/Sidebar.tsx'
-import ProductGrid from './features/product/ProductGrid.tsx'
-import ShippingBenefits from './features/shipping/ShippingBenefits.tsx'
-import ScrollToTop from './components/common/ScrollToTop.tsx'
+import Footer from './components/layout/Footer'
+import Header from './components/layout/header/Header'
+import PromoBanner from './components/ui/banner/PromoBanner'
+import Sidebar from './components/layout/Sidebar'
+import ProductGrid from './features/product/ProductGrid'
+import ShippingBenefits from './features/shipping/ShippingBenefits'
+import ScrollToTop from './components/common/ScrollToTop'
 
 function App() {
   const productGridRef = useRef<HTMLDivElement>(null)
